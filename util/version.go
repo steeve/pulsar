8ee7f9d7efaf3bab14be30dd33ef365195f430c5
Steeve Morin <steeve.morin@gmail.com>
2014-10-14 15:29:29 +0200

    Centralize User-Agent generationg
    Signed-off-by: Steeve Morin <steeve.morin@gmail.com>

--- util/version.go
+++ util/version.go
@@ -1,6 +1,16 @@
 package util
 
+import (
+	"fmt"
+
+	"github.com/steeve/libtorrent-go"
+)
+
 var (
 	GitCommit string
 	Version   string
 )
+
+func UserAgent() string {
+	return fmt.Sprintf("Pulsar/%s libtorrent/%s", Version, libtorrent.LIBTORRENT_VERSION)
+}
