47989439f3559973370c16a943df8ad024678995
Steeve Morin <steeve.morin@gmail.com>
2014-11-26 21:32:48 +0100

    Make normalization better
    Signed-off-by: Steeve Morin <steeve.morin@gmail.com>

--- providers/util.go
+++ providers/util.go
@@ -9,6 +9,18 @@ import (
 	"code.google.com/p/go.text/unicode/norm"
 )
 
+var (
+	trailingApostrophe = regexp.MustCompile(`'([a-z]{1,2}\s)`)
+)
+
+func RemoveTrailingApostrophe(str string) string {
+	return trailingApostrophe.ReplaceAllString(str, "$1")
+}
+
+func RemoveTrailingApostrophes(str string) string {
+	return ""
+}
+
 // as per http://en.wikipedia.org/wiki/Hepburn_romanization#Variations
 func RomanizeHepburn(str string) string {
 	str = strings.Replace(str, "ō", "ou", -1)
@@ -20,6 +32,8 @@ func NormalizeTitle(title string) string {
 	normalizedTitle := title
 	normalizedTitle = strings.ToLower(normalizedTitle)
 	normalizedTitle = RomanizeHepburn(title)
+	normalizedTitle = strings.ToLower(normalizedTitle)
+	normalizedTitle = RemoveTrailingApostrophe(normalizedTitle)
 	normalizedTitle, _, _ = transform.String(transform.Chain(
 		norm.NFD,
 		transform.RemoveFunc(func(r rune) bool {
@@ -29,7 +43,7 @@ func NormalizeTitle(title string) string {
 	normalizedTitle = strings.ToLower(normalizedTitle)
 	normalizedTitle = regexp.MustCompile(`\(\d+\)`).ReplaceAllString(normalizedTitle, " ")
 	normalizedTitle = strings.Map(func(r rune) rune {
-		if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
+		if !unicode.IsLetter(r) && !unicode.IsDigit(r) && r != '.' {
 			return ' '
 		}
 		return r
