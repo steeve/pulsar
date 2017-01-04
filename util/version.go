c7eb18c8dd7c4688d0f2aadad79dfa2c8cdc9d91
scakemyer <scakemyer@tuta.io>
2017-01-04 02:42:18 -0500

    get libtorrent version directly

--- util/version.go
+++ util/version.go
@@ -1,11 +1,14 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/scakemyer/libtorrent-go"
+)
 
 var (
 	Version   string
 )
 
 func UserAgent() string {
-	return fmt.Sprintf("Quasar/%s libtorrent/%s", Version[1:len(Version) - 1], "v1.1.1")
+	return fmt.Sprintf("Quasar/%s libtorrent/%s", Version[1:len(Version) - 1], libtorrent.Version())
 }
